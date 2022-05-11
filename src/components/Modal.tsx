8e23112c1659c4e5062c980bf697726755da38bf
francis <francis@aahcs.com>
2022-05-11 18:25:52 -0400

    feat: implement close button on modal. Implement classNames library

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,21 @@
 import { useRef } from 'react';
+import classNames from 'classnames';
 import useOnClickOutside from 'components/hooks/useOnOutsideClick';
 
 interface ModalProps {
   handleCloseModal: () => void;
   noCloseOutsideClick?: boolean;
+  children: React.ReactNode;
+  bgColor?: string;
 }
 
 export default function Modal(props: ModalProps) {
-  const { handleCloseModal, noCloseOutsideClick = false } = props;
+  const {
+    handleCloseModal,
+    noCloseOutsideClick = false,
+    bgColor,
+    children,
+  } = props;
 
   const modalRef = useRef(null);
 
@@ -15,15 +23,43 @@ export default function Modal(props: ModalProps) {
 
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none my-6 mx-auto max-w-3xl">
+      <div
+        role="dialog"
+        id="dialog"
+        aria-modal="true"
+        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none my-6 mx-auto max-w-4xl"
+      >
         <div
           ref={modalRef}
-          className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+          className={classNames(
+            `flex border-b border-solid border-slate-200 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none ${
+              bgColor && `bg-${bgColor}`
+            }`
+          )}
         >
-          <h3 className="text-3xl font-semibold">Modal Title</h3>
+          <button
+            type="button"
+            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+            data-modal-toggle="authentication-modal"
+            onClick={handleCloseModal}
+          >
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          {children}
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black" />
+      <div className="opacity-50 fixed inset-0 z-40 bg-black" />
     </>
   );
 }
