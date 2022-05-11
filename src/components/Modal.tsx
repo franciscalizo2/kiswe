6f6572db58e4e61ce1d3f75ec02c154c6b2a919c
francis <francis@aahcs.com>
2022-05-11 18:32:05 -0400

    feat: modal responsiveness

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -27,12 +27,12 @@ export default function Modal(props: ModalProps) {
         role="dialog"
         id="dialog"
         aria-modal="true"
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none my-6 mx-auto max-w-4xl"
+        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none sm:my-6 mx-auto max-w-4xl"
       >
         <div
           ref={modalRef}
           className={classNames(
-            `flex border-b border-solid border-slate-200 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none ${
+            `flex border-b border-solid border-slate-200 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none h-full sm:h-auto ${
               bgColor && `bg-${bgColor}`
             }`
           )}
