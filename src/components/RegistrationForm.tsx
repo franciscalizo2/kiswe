df59d66b1e468ead66f024b50cc5c501a72efbc2
francis <francis@aahcs.com>
2022-05-11 19:58:01 -0400

    feat: [WIP] implement validation for form fields

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -1,9 +1,78 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 export default function RegistrationForm() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      confirmEmail: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+      willReceiveEmails: false,
+      hasAgreedToTerms: false,
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email('Please enter a valid email address')
+        .required('Email is required'),
+      confirmEmail: Yup.string()
+        .email('Please enter a valid email address')
+        .oneOf([Yup.ref('email'), null], 'Email and Confirm Email must match')
+        .required('Email is required'),
+      username: Yup.string()
+        .min(3, 'Username must be at least 3 characters long')
+        .max(20, 'Username must be less than 20 characters long')
+        .required('Username is required'),
+      // TODO: Can only at most 1 punctuation chracter
+      // .matches(
+      //   /^'?\p{L}+(?:[' ]\p{L}+)*'?$/u,
+      //   'Username can only contain one punctuation character'
+      // )
+      password: Yup.string()
+        .min(8, 'Password must be at least 8 characters long')
+        .required('Password is required')
+        .matches(
+          /^(?=.*[a-z])/,
+          'Password must contain at least one lowercase character'
+        )
+        .matches(
+          /^(?=.*[A-Z])/,
+          'Password must contain at least one uppercase character'
+        )
+        .matches(/^(?=.*[0-9])/, 'Password must contain at least one number'),
+      confirmPassword: Yup.string()
+        .oneOf(
+          [Yup.ref('password'), null],
+          'Password and Confirm Password must match'
+        )
+        .min(8, 'Password must be at least 8 characters long')
+        .required('Password is required')
+        .matches(
+          /^(?=.*[a-z])/,
+          'Password must contain at least one lowercase character'
+        )
+        .matches(
+          /^(?=.*[A-Z])/,
+          'Password must contain at least one uppercase character'
+        )
+        .matches(/^(?=.*[0-9])/, 'Password must contain at least one number'),
+      hasAgreedToTerms: Yup.boolean().oneOf(
+        [true],
+        'Please agree to the all the terms and conditions'
+      ),
+    }),
+    onSubmit: (values) => {
+      // TODO: Handle Submission
+      console.log(values);
+    },
+  });
+
   return (
     <div className="pt-6 pb-12 px-16 text-white w-full max-w-[870px]">
       <h2 className="mt-6 mb-3 text-center font-light">Create a new account</h2>
 
-      <form>
+      <form onSubmit={formik.handleSubmit} noValidate>
         <div className="grid grid-cols-1 gap-5">
           <div>
             <label htmlFor="email" className="registration-label">
@@ -13,8 +82,17 @@ export default function RegistrationForm() {
               type="email"
               id="email"
               name="email"
-              className="registration-text-input"
+              onChange={formik.handleChange}
+              className={`registration-text-input ${
+                formik.errors.email &&
+                formik.touched.email &&
+                'border border-red-500'
+              }`}
             />
+
+            {formik.errors.email && formik.touched.email && (
+              <p className="registration-error-text">{formik.errors.email}</p>
+            )}
           </div>
 
           <div>
@@ -25,8 +103,19 @@ export default function RegistrationForm() {
               type="email"
               id="confirmEmail"
               name="confirmEmail"
-              className="registration-text-input"
+              onChange={formik.handleChange}
+              className={`registration-text-input ${
+                formik.errors.confirmEmail &&
+                formik.touched.confirmEmail &&
+                'border border-red-500'
+              }`}
             />
+
+            {formik.errors.confirmEmail && formik.touched.confirmEmail && (
+              <p className="registration-error-text">
+                {formik.errors.confirmEmail}
+              </p>
+            )}
           </div>
 
           <div>
@@ -37,8 +126,19 @@ export default function RegistrationForm() {
               type="text"
               id="username"
               name="username"
-              className="registration-text-input"
+              onChange={formik.handleChange}
+              className={`registration-text-input ${
+                formik.errors.username &&
+                formik.touched.username &&
+                'border border-red-500'
+              }`}
             />
+
+            {formik.errors.username && formik.touched.username && (
+              <p className="registration-error-text">
+                {formik.errors.username}
+              </p>
+            )}
           </div>
 
           <div>
@@ -49,8 +149,19 @@ export default function RegistrationForm() {
               type="password"
               id="password"
               name="password"
-              className="registration-text-input"
+              onChange={formik.handleChange}
+              className={`registration-text-input ${
+                formik.errors.password &&
+                formik.touched.password &&
+                'border border-red-500'
+              }`}
             />
+
+            {formik.errors.password && formik.touched.password && (
+              <p className="registration-error-text">
+                {formik.errors.password}
+              </p>
+            )}
           </div>
 
           <div>
@@ -61,8 +172,20 @@ export default function RegistrationForm() {
               type="password"
               id="confirmPassword"
               name="confirmPassword"
-              className="registration-text-input"
+              onChange={formik.handleChange}
+              className={`registration-text-input ${
+                formik.errors.confirmPassword &&
+                formik.touched.confirmPassword &&
+                'border border-red-500'
+              }`}
             />
+
+            {formik.errors.confirmPassword &&
+              formik.touched.confirmPassword && (
+                <p className="registration-error-text">
+                  {formik.errors.confirmPassword}
+                </p>
+              )}
           </div>
 
           <div className="flex">
@@ -70,6 +193,7 @@ export default function RegistrationForm() {
               type="checkbox"
               id="willReceiveEmails"
               name="willReceiveEmails"
+              onChange={formik.handleChange}
             />
             <label
               htmlFor="willReceiveEmails"
@@ -85,6 +209,7 @@ export default function RegistrationForm() {
               type="checkbox"
               id="hasAgreedToTerms"
               name="hasAgreedToTerms"
+              onChange={formik.handleChange}
             />
             <label
               htmlFor="hasAgreedToTerms"
@@ -118,8 +243,8 @@ export default function RegistrationForm() {
           <div className="mb-3">
             <button
               type="submit"
-              disabled
               className="inline bg-primary-500 text-white px-3 py-2 font-semibold rounded-md text-sm text-center disabled:opacity-50"
+              disabled={!(formik.isValid && formik.dirty)}
             >
               Submit
             </button>
